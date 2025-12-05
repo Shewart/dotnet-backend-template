93bfb259f44d3310e89809aa001784a783b64802
Shewart <shewartshepherd23@gmail.com>
2025-12-06 01:54:56 +0200

    feat: Add DTOs for product creation and update requests to facilitate API interactions

---
+++ Dotnet.Api/DTOs/UpdateProductRequest.cs
@@ -0,0 +1,10 @@
+namespace Dotnet.Api.DTOs;
+
+public class UpdateProductRequest
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public required decimal Price { get; set; }
+    public required string Category { get; set; }
+    public bool InStock { get; set; }
+}
